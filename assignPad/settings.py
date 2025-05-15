b212de67fe8cfcf69be9b26b6b2351e952973c1e
MOHAMMAD FAIZAN ASHRAF <134165364+ProgrammingTechniques@users.noreply.github.com>
2025-05-15 20:43:34 +0530

    DONE

--- assignPad/settings.py
+++ assignPad/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 
 INSTALLED_APPS = [
