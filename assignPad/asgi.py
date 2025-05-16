81868bbd8d469604dd4b76d01f8dd5424d4eeb9d
MOHAMMAD FAIZAN ASHRAF <134165364+ProgrammingTechniques@users.noreply.github.com>
2025-05-16 18:24:01 +0530

    DONE

--- assignPad/asgi.py
+++ assignPad/asgi.py
@@ -1,19 +1,18 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assignPad.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assignPad.settings")
+
+django.setup()  # Ye line add karo yaha
 
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter , URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from chat import routing
 
 application = ProtocolTypeRouter(
     {
-        "http" : get_asgi_application() , 
-        "websocket" : AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
-            )    
-        )
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
     }
 )
